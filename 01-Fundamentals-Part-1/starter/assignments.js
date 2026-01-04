7d257c513d706787762b2e60c9507411e6efc00d
Eddy G <eddyguzman322@gmail.com>
2026-01-04 15:11:45 -0500

    Jan 4th practice

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// ********* Values and Variables Assignment *********
+let country = 'United States of America';
+let continent = 'North America';
+let population = 340;
+
+console.log(country);
+console.log(continent);
+console.log(population);
