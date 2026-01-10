8740f6b28c8c2e76cd1a0c7e5750ce1c401d0261
Eddy G <eddyguzman322@gmail.com>
2026-01-10 10:38:11 -0500

    Course work 1/10

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,8 +1,21 @@
 // ********* Values and Variables Assignment *********
-let country = 'United States of America';
-let continent = 'North America';
+const country = 'United States of America';
+const continent = 'North America';
 let population = 340;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// ********* Data Type *********
+// ********* let, const, and var *********
+
+const isIsland = false;
+let language;
+language = 'English';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof lagnauge);
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/* Values and Variables
       let js = 'amazing';
       console.log(40+8+23-10);
 
@@ -10,6 +11,7 @@
       console.log(firstName);
       console.log(firstName);
 
+      // Variable name conventions
       let jonas_matilda = "JM";
       let $function = 27;
 
@@ -23,3 +25,86 @@
       let job2 = 'teacher';
 
       console.log(myFirstJob);
+*/
+
+/* Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun)
+//console.log(typeof 23)
+//console.log(typeof 'Jonas')
+
+javascriptIsFun = 'YES!'; // example of dynamic typing
+console.log(typeof javascriptIsFun)
+
+let year; // example of undefined 
+console.log(year)
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31; 
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Guzman';
+console.log(lastName);
+*/
+
+/* Basic Operators 
+// Math Operators  
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah,);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// Concatenation of strings 
+const firstName = 'Jonas';
+const lastName = 'Guzman';
+console.log(firstName + ' ' + lastName); 
+
+// Assignment Operators 
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // 25 * 4 = 100
+x++; // 100 + 1 = 101 
+x--; // 101 - 1 = 100 
+x--; // 100 - 1 = 99
+console.log(x);
+
+// Comparison Operators 
+console.log(ageJonas > ageSarah);// >, <, >=, <= 
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25-10-5); // example of left to right execution
+
+let x, y;
+x = y = 25 - 10 - 5; // example of right to left execution 
+// x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = ageJonas + ageSarah / 2;
+console.log(ageJonas, ageSarah);
