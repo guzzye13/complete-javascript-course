5598f3f787a896a653b8f9d75552d92a152995ab
Eddy G <eddyguzman322@gmail.com>
2026-02-07 16:10:52 -0500

    temp changes

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+// Activating Strict mode 
+// makes it easier to write secure JS code and must write at beginning of script 
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = 'true';
+if(hasDriversLicense) console.log('test');
+
+// const interface = 'Audio';
+// const private = 534;
